5ebb341871b0667a99991770d9bdd14c347a9734
Frank Arlt <frank.arlt@valtech.com>
2022-07-11 11:13:37 +0200

    Created API functions

---
+++ src/api/albums.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { ALBUMS_API_URL, PHOTOS_API_URL } from "../config/variables";
+
+export const getAlbums = (userId) =>
+  axios
+    .get(ALBUMS_API_URL + "?userId=" + userId)
+    .then((response) => response.data)
+    .catch((error) => {
+      console.log(error);
+    });
+
+export const getPhotos = (albumId) =>
+  axios
+    .get(PHOTOS_API_URL + "?albumId=" + albumId)
+    .then((response) => response.data)
+    .catch((error) => {
+      console.log(error);
+    });
